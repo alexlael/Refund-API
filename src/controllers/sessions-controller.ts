3fe48b27b05b7d670da210b56bf551f5242014bc
Alex Lael <alex.lael.bsb@gmail.com>
2025-07-14 15:32:50 -0300

    feat: enhance session creation logic with validation and error handling

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,13 +1,34 @@
-import { Request, Response } from 'express';
-import { AppError } from '@/utils/AppError';
+import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/database/prisma";
+import { compare } from "bcrypt";
+import { z } from "zod";
 
 class SessionsController {
-    async create(request: Request, response:Response){
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email("Invalid email format"),
+      password: z.string(),
+    });
 
-        response.json({message: "Ok"})
+    const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new AppError("Credentials are not valid", 401);
     }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError("Credentials are not valid", 401);
+    }
+
+    response.json({ email, password });
+  }
 }
 
-
-
-export {SessionsController};
+export { SessionsController };

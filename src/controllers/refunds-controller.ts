30a2d11da7b79035d352e8ba333dd4fdee5885a0
Alex Lael <alex.lael.bsb@gmail.com>
2025-07-16 22:17:47 -0300

    feat: implement validation for refund creation with Zod schema

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,7 +1,36 @@
+import { Category } from "@prisma/client";
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+import { z } from "zod";
+
+const CategoriesEnum = z.enum(["food", "others", "services", "transport"]);
 
 class RefundsController {
   async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().min(1, "Name is required"),
+      category: CategoriesEnum,
+      amount: z.number().min(0.01, "Amount must be greater than 0"),
+      filename: z.string().min(20, "Filename is required"),
+    });
+
+    const { name, category, amount, filename } = bodySchema.parse(request.body);
+
+    if (!request.user?.id) {
+      throw new AppError("User not authenticated", 401);
+    }
+
+    const refund = await prisma.refunds.create({
+      data: {
+        name,
+        category,
+        amount,
+        filename,
+        userId: request.user.id,
+      },
+    });
+
     return response.json({ message: "passou" });
   }
 }
